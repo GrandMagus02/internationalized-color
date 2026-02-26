5e0f56da63ff869e90cc41060779fe194d25f6fa
GrandMagus <grandmagusdanilka@gmail.com>
2026-02-26 23:16:56 +0200

    refactor: replace modes array with individual useMode exports for color modes

--- src/bootstrap/css.ts
+++ src/bootstrap/css.ts
@@ -25,24 +25,19 @@ import {
   modeLrgb,
 } from 'culori/fn';
 
-const modes = [
-  modeRgb,
-  modeHsv,
-  modeHsl,
-  modeHwb,
-  modeLab,
-  modeLch,
-  modeOklab,
-  modeOklch,
-  modeP3,
-  modeA98,
-  modeProphoto,
-  modeRec2020,
-  modeXyz50,
-  modeXyz65,
-  modeLrgb,
-];
 
-for (const mode of modes) {
-  useMode(mode as any);
-}
+export const rgb = useMode(modeRgb);
+export const hsv = useMode(modeHsv);
+export const hsl = useMode(modeHsl);
+export const hwb = useMode(modeHwb);
+export const lab = useMode(modeLab);
+export const lch = useMode(modeLch);
+export const oklab = useMode(modeOklab);
+export const oklch = useMode(modeOklch);
+export const p3 = useMode(modeP3);
+export const a98 = useMode(modeA98);
+export const prophoto = useMode(modeProphoto);
+export const rec2020 = useMode(modeRec2020);
+export const xyz50 = useMode(modeXyz50);
+export const xyz65 = useMode(modeXyz65);
+export const lrgb = useMode(modeLrgb);
